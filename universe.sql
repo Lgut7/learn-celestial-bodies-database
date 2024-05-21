b94d273ba98db8f034cb5c6ab521e9d1b3e998e7
Lgut7 8 <gut-lukas@web.de>
2024-05-21 12:35:10 +0000

    completed

--- universe.sql
+++ universe.sql
@@ -265,65 +265,112 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.galaxy VALUES (1, 'Milchstraße', 8, true, 'Ringe');
+INSERT INTO public.galaxy VALUES (2, 'alpha1', 28, false, 'Schraube');
+INSERT INTO public.galaxy VALUES (3, 'beta2', 13, false, 'Ringe');
+INSERT INTO public.galaxy VALUES (4, 'gamma3', 63, true, 'Spirale');
+INSERT INTO public.galaxy VALUES (5, 'delat4', 44, true, 'Spirale');
+INSERT INTO public.galaxy VALUES (6, 'Bernd', 3, false, 'Reihe');
 
 
 --
 -- Data for Name: moon; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.moon VALUES (2, 'Trabant1', 'Rund', 23425, 1);
+INSERT INTO public.moon VALUES (3, 'Trabant2', 'Oval', 29796965, 1);
+INSERT INTO public.moon VALUES (4, 'Trabant3', 'Rechteckig', 96965, 1);
+INSERT INTO public.moon VALUES (5, 'Trabant4', '', 96965, 1);
+INSERT INTO public.moon VALUES (6, 'Trabant5', '', 234, 1);
+INSERT INTO public.moon VALUES (7, 'Trabant6', '', 1134651, 3);
+INSERT INTO public.moon VALUES (8, 'Trabant7', '', 143551, 7);
+INSERT INTO public.moon VALUES (9, 'Trabant8', '', 1677551, 10);
+INSERT INTO public.moon VALUES (11, 'Trabant9', '', 1677551, 10);
+INSERT INTO public.moon VALUES (12, 'Trabant10', '', 1677551, 10);
+INSERT INTO public.moon VALUES (15, 'Trabant11', '', 13457551, 14);
+INSERT INTO public.moon VALUES (16, 'Trabant12', '', 13457551, 13);
+INSERT INTO public.moon VALUES (17, 'Trabant13', '', 13457551, 13);
+INSERT INTO public.moon VALUES (18, 'Trabant14', '', 13457551, 13);
+INSERT INTO public.moon VALUES (19, 'Trabant15', '', 13457551, 13);
+INSERT INTO public.moon VALUES (20, 'Trabant16', '', 166666, 8);
+INSERT INTO public.moon VALUES (21, 'Trabant17', '', 166666, 7);
+INSERT INTO public.moon VALUES (22, 'Trabant18', '', 34266, 3);
+INSERT INTO public.moon VALUES (23, 'Trabant19', '', 32777766, 3);
+INSERT INTO public.moon VALUES (24, 'Trabant20', '', 3345996, 1);
 
 
 --
 -- Data for Name: planet; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.planet VALUES (1, 'Erde', true, 12742.00, 1);
+INSERT INTO public.planet VALUES (3, 'Jupiter', true, 234552.00, 4);
+INSERT INTO public.planet VALUES (4, 'Mars', false, 93475.00, 3);
+INSERT INTO public.planet VALUES (5, 'Saturn2', false, 234234.00, 2);
+INSERT INTO public.planet VALUES (6, 'Venus1', false, 11111.00, 1);
+INSERT INTO public.planet VALUES (7, 'Venus2', false, 111234.00, 2);
+INSERT INTO public.planet VALUES (8, 'Merkus4', true, 41234.00, 4);
+INSERT INTO public.planet VALUES (10, 'Galaxus', true, 923554.00, 3);
+INSERT INTO public.planet VALUES (11, 'Crypton', true, 3242344.00, 2);
+INSERT INTO public.planet VALUES (12, 'Pluto', true, 1.00, 1);
+INSERT INTO public.planet VALUES (13, 'Neptun', true, 2345.00, 1);
+INSERT INTO public.planet VALUES (14, 'Mordor', false, 34565.00, 7);
 
 
 --
 -- Data for Name: spaceprobe; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.spaceprobe VALUES (1, 'MIR', 7);
+INSERT INTO public.spaceprobe VALUES (2, 'ISS', 12);
+INSERT INTO public.spaceprobe VALUES (3, 'SPUTNIK', 5);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.star VALUES (1, 'Sonne', 23478688, 87835.23, 1);
+INSERT INTO public.star VALUES (2, 'Solaris', 2432668, 23435.23, 4);
+INSERT INTO public.star VALUES (3, 'Zwerg', 11111, 11111.33, 2);
+INSERT INTO public.star VALUES (4, 'Sauron', 1234567, 34584.63, 3);
+INSERT INTO public.star VALUES (6, 'Solaris2', 134534252, 300005.43, 6);
+INSERT INTO public.star VALUES (7, 'Solaris3', 2345252, 12345.43, 5);
 
 
 --
 -- Name: galaxy_galaxy_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 1, false);
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 6, true);
 
 
 --
 -- Name: moon_moon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.moon_moon_id_seq', 1, false);
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 24, true);
 
 
 --
 -- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.planet_planet_id_seq', 1, false);
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 14, true);
 
 
 --
 -- Name: spaceprobe_spaceprobe_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.spaceprobe_spaceprobe_id_seq', 1, false);
+SELECT pg_catalog.setval('public.spaceprobe_spaceprobe_id_seq', 3, true);
 
 
 --
 -- Name: star_star_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.star_star_id_seq', 1, false);
+SELECT pg_catalog.setval('public.star_star_id_seq', 7, true);
 
 
 --
